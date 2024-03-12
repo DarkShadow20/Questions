1b88a7f1cfb6e7ae488bf17b444d196b48476be5
DarkShadow20 <kunal8011@gmail.com>
2024-03-12 15:41:35 +0530

    added solution to 1662

---
+++ 1662.CheckIfTwoStringArraysareEquivalent.js
@@ -0,0 +1,9 @@
+var arrayStringsAreEqual = function(word1, word2) {
+    const string1 = word1.join("")
+    const string2 = word2.join("")
+    if(string1 == string2){
+        return true;
+    }
+    return false
+};
+
