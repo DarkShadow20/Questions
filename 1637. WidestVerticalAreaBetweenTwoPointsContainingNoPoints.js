c875a757b139821e8129aa6d88df9bde7298701a
DarkShadow20 <kunal8011@gmail.com>
2024-03-15 09:58:41 +0530

    added solution for 1637

---
+++ 1637. WidestVerticalAreaBetweenTwoPointsContainingNoPoints.js
@@ -0,0 +1,12 @@
+var maxWidthOfVerticalArea = function(points) {
+    points.sort((a,b)=> a[0]-b[0])
+    
+    let ans = 0;
+    for(let i=1;i<points.length;i++){
+        if(ans < points[i][0] - points[i-1][0])
+        {
+            ans = points[i][0] - points[i-1][0]
+        }
+    }
+    return ans
+  };
