d99b3e3e1e5fb84b3c3f66c418aafa32608948e5
DarkShadow20 <kunal8011@gmail.com>
2024-04-01 18:58:45 +0530

    added solution to delNthNode

---
+++ removeNthNode.js
@@ -0,0 +1,16 @@
+var removeNthFromEnd = function(head, n) {
+    let fast = head
+    let slow = head
+    for(let i=0;i<n;i++){
+        fast = fast.next
+    }
+    if(fast == null){
+        return head.next
+    }
+    while(fast && fast.next!= null){
+        fast = fast.next
+        slow = slow.next
+    }
+    slow.next = slow.next.next
+    return head
+};
