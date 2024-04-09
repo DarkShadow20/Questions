3822709f1277ae1f175181b0de7813e394ee89b1
DarkShadow20 <kunal8011@gmail.com>
2024-04-09 15:17:51 +0530

    added solution to pallindrome linkedlist

---
+++ pallindromeLinkedList.js
@@ -0,0 +1,48 @@
+var reverseLinkedList = function(head){
+    if(head == null || head.next == null){
+        return head
+    }
+    let newHead = reverseLinkedList(head.next)
+    let front = head.next
+    front.next = head
+    head.next = null
+    return newHead;
+}
+
+
+var isPalindrome = function(head) {
+    if(head == null || head.next == null){
+        return head
+    }
+    let fast = head
+    let slow = head
+    while(fast.next != null && fast.next.next!=null){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    let newHead = reverseLinkedList(slow.next)
+    let first = head
+    let second = newHead
+    while(second!=null){
+        if(first.data != second.data){
+            reverseLinkedList(newHead)
+            return false
+        }
+        first = first.next
+        second = second.next
+    }
+    reverseLinkedList(newHead)
+    return true;
+};
+
+
+var isPalindrome = function(head) {
+    let straight = '';
+    let reversed = '';
+    while (head) {
+        straight += head.val;
+        reversed = head.val + reversed;
+        head = head.next;
+    };
+    return straight === reversed;
+};
