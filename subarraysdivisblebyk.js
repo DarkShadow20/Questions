df3e5b9e872b729ea83776af32b43bc711f21549
DarkShadow20 <kunal8011@gmail.com>
2024-04-15 16:56:54 +0530

    added solution to subArrayDivisibleByK

--- subarraysdivisblebyk.js
+++ subarraysdivisblebyk.js
@@ -1,5 +1,25 @@
 var subarraysDivByK = function(nums, k) {
-    
+    let size = nums.length
+    let arr = Array(size).fill(0)
+    arr[0] = nums[0]
+    for(let i =1;i<size;i++){
+        arr[i] = arr[i-1] + nums[i]
+    }
+    for(let i=0;i<size;i++){
+        arr[i] = (arr[i] + k) % k
+        if(arr[i] < 0){
+            arr[i] = (arr[i] + k) % k
+        }
+    }
+    let freq = Array(k).fill(0)
+    let ans = 0
+    for(let i=0;i<size;i++){
+        ans += freq[arr[i]]
+        freq[arr[i]]++
+        if(arr[i] == 0)
+            ans++
+    } 
+    return ans
 };
 
 
