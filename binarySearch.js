275fd1d2ec352dc90c6e1f3746e90fa4976ddaa2
DarkShadow20 <kunal8011@gmail.com>
2024-05-09 17:09:25 +0530

    code for binary search

--- binarySearch.js
+++ binarySearch.js
@@ -1,6 +1,18 @@
-var binarySearch = ()=>{
-
+var binarySearch = (arr,n,x)=>{
+    let lo=0;
+    let hi = n-1;
+    while(lo<=hi){
+        let mid = parseInt((lo+hi)/2)
+        if(arr[mid] == x)
+            return mid
+        else if(arr[mid] < x){
+            lo = mid + 1
+        }else{
+            hi = mid-1
+        }
+    }
+    return -1
 }
 
 
-console.log(binarySearch())
+console.log(binarySearch([1,2,3,4,5,6],6,4))
