d0e9c5272c733d8dc6ad151ad3e8634650365efc
DarkShadow20 <kunal8011@gmail.com>
2024-05-23 18:08:03 +0530

    added better approach to sorted sqaure

--- sortedSqaures.js
+++ sortedSqaures.js
@@ -1,3 +1,4 @@
+//O(NLOGN)
 var sortedSquares = function(nums) {
     let n = nums.length
     let res = []
@@ -6,4 +7,28 @@ var sortedSquares = function(nums) {
     }
     res.sort((a,b)=>a-b)
     return res
+};
+
+//better approach
+
+//O(N)
+var sortedSquares = function(nums) {
+    let n = nums.length
+    let res = []
+    let index = n-1
+    let lastIndex = n-1;
+    let firstIndex = 0;
+    while(firstIndex<=lastIndex){
+        let sqOfLeft = nums[firstIndex] * nums[firstIndex]
+        let sqOfRight = nums[lastIndex] * nums[lastIndex]
+        if(sqOfRight > sqOfLeft){
+            res[index] = sqOfRight;
+            lastIndex--
+        }else{
+            res[index] = sqOfLeft
+            firstIndex++
+        }
+        index--
+    }
+    return res 
 };
