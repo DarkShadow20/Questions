8523c329ff613d91804521b8788a18684b5b553f
DarkShadow20 <kunal8011@gmail.com>
2024-05-31 16:04:21 +0530

    add solution to pallandromic substring

---
+++ pallandromicSubstrings.js
@@ -0,0 +1,27 @@
+var countSubstrings = function(s) {
+    let len = s.length
+    let result = s.length
+    for(let mid = 0; mid < len ; mid++){
+        let i = mid - 1
+        let j = mid + 1
+        while(i>=0 && j < len && s.charAt(i) == s.charAt(j)){
+            i--
+            j++
+            result++
+        }
+       
+    }
+    for(let mid = 0; mid < len; mid++){
+        let i = mid
+        let j = mid + 1
+        while(i>=0 && j<len && s.charAt(i) == s.charAt(j)){
+            i--
+            j++
+            result++
+        }
+    }
+    return result
+};
+
+
+console.log(countSubstrings("aaa"))
