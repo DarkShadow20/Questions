031e5c112ba2247e6b3b9e9672a96d85f2a108ff
DarkShadow20 <kunal8011@gmail.com>
2024-06-14 23:31:01 +0530

    add solution to contigous array

---
+++ 525. Contiguous Array.js
@@ -0,0 +1,18 @@
+var findMaxLength = function(nums) {
+    const map = new Map()
+    let maxLen = 0
+    let sum = 0
+    map.set(0,-1)
+    for(let i=0;i<nums.length;i++){
+        sum+=(nums[i] ==0)?1: -1
+        if(map.has(sum)){
+            maxLen = Math.max(i-map.get(sum),maxLen)
+        }else{
+            map.set(sum,i)
+        }
+    }
+    return maxLen
+};
+
+
+console.log(findMaxLength([0,0,0,1,0,0,1,1,1,0]))
