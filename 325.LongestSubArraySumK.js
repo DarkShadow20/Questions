cbc9947ff70d00322e25f7d1a840ee53550e4a14
DarkShadow20 <kunal8011@gmail.com>
2024-06-30 18:34:52 +0530

    added solution to 325

--- 325.LongestSubArraySumK.js
+++ 325.LongestSubArraySumK.js
@@ -1,7 +1,24 @@
 // Find the longest subarray with sum = k
 
 var longSubSumK = (nums,k) =>{
-    
+    let sum = 0
+    let map = new Map();
+    map.set(0,-1)
+    let maxLen = 0
+    for(let i=0;i<nums.length;i++){
+        // if(i==0){
+        //     prefixSum[i] = nums[i]
+        // }else{
+        //     prefixSum[i] = nums[i] + prefixSum[i-1]
+        // }
+        sum+=nums[i]
+        let valueNeedToSubtract = sum - k;
+        if(map.has(valueNeedToSubtract))
+            maxLen = Math.max(i-map.get(valueNeedToSubtract),maxLen)
+        if(!map.has(sum))
+            map.set(sum,i)
+    }
+    return maxLen
 }
 
 console.log(longSubSumK([1,-1,5,2,-1],6))
