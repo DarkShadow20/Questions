c17240bb644677c4edd1e88ad7266bd27122f48e
DarkShadow20 <kunal8011@gmail.com>
2024-07-02 10:51:47 +0530

    add another approach to group anagrams

--- Group Anagrams.js
+++ Group Anagrams.js
@@ -11,4 +11,25 @@ var groupAnagrams = function(strs) {
   return Object.values(groups)
 };
 
-console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
+//another approach
+var groupAnagram = function (strs){
+  let groups = {}
+  for(let str of strs){
+    let freq = new Array(26).fill(0)
+    let strLen = str.length
+    for(let i=0;i<strLen;i++){
+      freq[str.charCodeAt(i) - 'a'.charCodeAt(0)]++
+    }
+    let freqPattern = freq.join("#");
+    
+    if(!groups[freqPattern]){
+      groups[freqPattern] = []
+    }
+    groups[freqPattern].push(str)
+  }
+  console.log(groups)
+  return Object.values(groups)
+}
+//console.log(groupAnagram(["eat","tea","tan","ate","nat","bat"]))
+console.log(groupAnagram(["bdddddddddd","bbbbbbbbbbc"]))
