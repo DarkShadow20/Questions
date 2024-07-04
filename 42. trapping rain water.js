0f69e1b463af4a2633103882d4eac10dab71624d
DarkShadow20 <kunal8011@gmail.com>
2024-07-04 17:07:51 +0530

    add solution to 42

---
+++ 42. trapping rain water.js
@@ -0,0 +1,22 @@
+var trap = function(height) {
+    let n = height.length
+    let leftMax = []
+    let rightMax = []
+    let ans = 0
+    leftMax[0] = height[0]
+    rightMax[n-1] = height[n-1] 
+    for(let i=1;i<n;i++){
+        leftMax[i] = Math.max(leftMax[i-1],height[i])
+    }
+    for(let i = n-2;i>=0;i--){
+        rightMax[i] = Math.max(rightMax[i+1],height[i])
+    }
+    for(let i = 0;i<n;i++){
+        ans += Math.min(leftMax[i],rightMax[i]) - height[i]
+    }
+    return ans
+};
+
+//optimization not leftmax
+
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
