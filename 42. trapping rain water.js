2f0ddd24392fcead8c4389bed007b6815fc36c82
DarkShadow20 <kunal8011@gmail.com>
2024-07-04 17:24:29 +0530

    added space optimization

--- 42. trapping rain water.js
+++ 42. trapping rain water.js
@@ -18,5 +18,21 @@ var trap = function(height) {
 };
 
 //optimization not leftmax
+var trap1 = function(height) {
+    let n = height.length
+    let leftMax = 0
+    let rightMax = []
+    let ans = 0
+    rightMax[n-1] = height[n-1] 
 
-console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+    for(let i = n-2;i>=0;i--){
+        rightMax[i] = Math.max(rightMax[i+1],height[i])
+    }
+    for(let i = 0;i<n;i++){
+        leftMax = Math.max(leftMax,height[i])
+        ans += Math.min(leftMax,rightMax[i]) - height[i]
+    }
+    return ans
+};
+
+console.log(trap1([0,1,0,2,1,0,1,3,2,1,2,1]))
