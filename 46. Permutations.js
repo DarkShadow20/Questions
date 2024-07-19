e7d650732f412248a02e71d68fa6c0d42b654263
DarkShadow20 <kunal8011@gmail.com>
2024-07-19 12:27:03 +0530

    added solution to 46

--- 46. Permutations.js
+++ 46. Permutations.js
@@ -1,19 +1,30 @@
 let premutations = []
-var helper = function(nums,index){
-    let result = []
+
+var swap = function ( nums , a , b ){
+    let t = nums[a]
+    nums[a] = nums[b]
+    nums[b] = t
+}
+
+var helper = function(nums,index){    
     if(index == nums.length - 1){
+        let permutation = []
         for(let x of nums){
-            result.push(x)
+            permutation.push(x)
         }
-        premutations.push(result)
+        premutations.push(permutation)
     }else{
         for(let i=index;i<nums.length;i++){
             swap(nums,i,index)
             helper(nums,index+1)
+            //backtracking (bringing back to the original state)
+            swap(nums,i,index)
         }
     }
 }
 
 var permute = function(nums){
+    premutations = []
     helper(nums,0)
+    return premutations
 }
